a757219df8b053ede08f317dd7aa12638f418e49
João Caetano <jocaeneto@hotmail.com>
2026-04-13 23:45:55 +0300

    Initial Commit

---
+++ Citisen/CitisenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitisenApp.swift
+//  Citisen
+//
+//  Created by Joao Caetano on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CitisenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
